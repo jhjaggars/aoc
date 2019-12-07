413b982888844f0476acab8b2439afce223e8543
Jesse Jaggars <jjaggars@redhat.com>
2019-12-07 14:05:08 -0600

    day 7
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

---
+++ 2019/seven/src/main.rs
@@ -0,0 +1,370 @@
+use permutohedron::heap_recursive;
+use std::collections::VecDeque;
+use std::fs;
+
+#[derive(Debug)]
+struct Op {
+    code: usize,
+    mode1: usize,
+    mode2: usize,
+    mode3: usize,
+}
+
+impl Op {
+    fn param(&self, state: &Vec<i64>, offset: usize, param: usize) -> i64 {
+        let actual = state[offset + param];
+        let mode = match param {
+            1 => self.mode1,
+            2 => self.mode2,
+            3 => self.mode3,
+            _ => unreachable!("oh jesus!"),
+        };
+        if mode == 1 {
+            actual
+        } else {
+            state[actual as usize]
+        }
+    }
+}
+
+fn get_opcode(input: i64) -> Op {
+    Op {
+        code: (input % 100) as usize,
+        mode1: ((input / 100) % 10) as usize,
+        mode2: ((input / 1000) % 10) as usize,
+        mode3: ((input / 10000) % 10) as usize,
+    }
+}
+
+#[derive(Debug)]
+struct ICResult {
+    output: i64,
+    done: bool,
+}
+
+fn run(state: &mut Vec<i64>, offset: &mut usize, input: Vec<i64>) -> ICResult {
+    // println!("running {:?} with {:?}", state, input);
+    let mut output = ICResult {
+        output: 0,
+        done: false,
+    };
+    let mut in_q = VecDeque::from(input);
+    loop {
+        let op = get_opcode(state[*offset]);
+        // println!("offset is {}", offset);
+        // println!("\tstate is {:?}", state);
+        // println!("\top is {:?}", op);
+
+        if op.code == 99 {
+            output.done = true;
+            break;
+        }
+
+        match op.code {
+            1 => {
+                let left = op.param(state, *offset, 1);
+                let right = op.param(state, *offset, 2);
+                let dest = state[*offset + 3] as usize;
+                state[dest] = left + right;
+                *offset += 4;
+            }
+            2 => {
+                let left = op.param(state, *offset, 1);
+                let right = op.param(state, *offset, 2);
+                let dest = state[*offset + 3] as usize;
+                state[dest] = left * right;
+                *offset += 4;
+            }
+            3 => {
+                let dest = state[*offset + 1] as usize;
+                match in_q.pop_front() {
+                    Some(v) => {
+                        // println!("reading input: {}", v);
+                        state[dest] = v;
+                    }
+                    None => break,
+                }
+                *offset += 2;
+            }
+            4 => {
+                output.output = op.param(state, *offset, 1);
+                // println!("setting output to {:?}", output);
+                *offset += 2;
+            }
+            5 => {
+                let left = op.param(state, *offset, 1);
+                let right = op.param(state, *offset, 2);
+                // println!("\t {} != 0", left);
+                if left != 0 {
+                    // println!("\t * {} neq 0, jumping to {}", left, right);
+                    *offset = right as usize;
+                } else {
+                    *offset += 3;
+                }
+            }
+            6 => {
+                let left = op.param(state, *offset, 1);
+                let right = op.param(state, *offset, 2);
+                if left == 0 {
+                    // println!("\t * {} eq 0, jumping to {}", left, right);
+                    *offset = right as usize;
+                } else {
+                    *offset += 3;
+                }
+            }
+            7 => {
+                let left = op.param(state, *offset, 1);
+                let right = op.param(state, *offset, 2);
+                let dest = state[*offset + 3] as usize;
+                state[dest] = if left < right { 1 } else { 0 };
+                *offset += 4;
+            }
+            8 => {
+                let left = op.param(state, *offset, 1);
+                let right = op.param(state, *offset, 2);
+                let dest = state[*offset + 3] as usize;
+                // println!("\t * storing {} == {} into {}", left, right, dest);
+                state[dest] = if left == right { 1 } else { 0 };
+                *offset += 4;
+            }
+            _ => unreachable!("unknown op code!"),
+        };
+    }
+    // println!("output is {:?}", output);
+    output
+}
+
+fn parse(value: String) -> Vec<i64> {
+    value
+        .split(",")
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect()
+}
+
+struct Amp {
+    state: Vec<i64>,
+    phase: i64,
+    offset: usize,
+    running: bool,
+    done: bool,
+}
+
+impl Amp {
+    fn exec(&mut self, input: i64) -> ICResult {
+        let r = match self.running {
+            true => run(&mut self.state, &mut self.offset, vec![input]),
+            false => {
+                self.running = true;
+                run(&mut self.state, &mut self.offset, vec![self.phase, input])
+            }
+        };
+        self.done = r.done;
+        r
+    }
+}
+
+fn run_phases(state: &mut Vec<i64>, phases: &[i64]) -> i64 {
+    let mut amps: Vec<Amp> = phases
+        .iter()
+        .clone()
+        .map(|p| Amp {
+            state: state.clone(),
+            phase: *p,
+            offset: 0,
+            done: false,
+            running: false,
+        })
+        .collect();
+    let mut out = 0;
+    let mut done = false;
+    loop {
+        for amp in &mut amps {
+            let r = amp.exec(out);
+            out = r.output;
+            done = r.done;
+        }
+        if done {
+            break;
+        }
+    }
+    out
+}
+
+fn main() {
+    let program = parse(fs::read_to_string("input.txt").unwrap().trim().to_string());
+    let mut signal = 0;
+    heap_recursive(&mut vec![9, 8, 7, 6, 5], |p| {
+        // println!("trying permuation {:?}", p);
+        let s = run_phases(&mut program.clone(), p);
+        if s > signal {
+            // println!("s({}) > signal({})", s, signal);
+            signal = s;
+        }
+    });
+    println!("largest signal was: {}", signal);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_big() {
+        let program = vec![
+            3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1,
+            28, 1005, 28, 6, 99, 0, 0, 5,
+        ];
+        assert_eq!(
+            run_phases(&mut program.clone(), &vec![9, 8, 7, 6, 5]),
+            139629729
+        );
+    }
+
+    #[test]
+    fn test_long() {
+        let program = vec![
+            3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54,
+            -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4,
+            53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10,
+        ];
+        assert_eq!(
+            run_phases(&mut program.clone(), &vec![9, 7, 8, 5, 6]),
+            18216
+        );
+    }
+
+    #[test]
+    fn test_43210() {
+        let program = vec![
+            3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0,
+        ];
+        assert_eq!(
+            run_phases(&mut program.clone(), &vec![4, 3, 2, 1, 0]),
+            43210
+        );
+    }
+
+    #[test]
+    fn test_cat() {
+        let mut program = parse(String::from("3,0,4,0,99"));
+        let mut offset = 0;
+        let output = run(&mut program, &mut offset, vec![1]).output;
+        assert_eq!(output, 1i64);
+    }
+
+    #[test]
+    fn test_multiply() {
+        let mut program = parse(String::from("1002,4,3,4,33"));
+        let mut offset = 0;
+        let _output = run(&mut program, &mut offset, vec![1]).output;
+    }
+
+    #[test]
+    fn test_negative() {
+        let mut program = parse(String::from("1101,100,-1,4,0"));
+        let mut offset = 0;
+        let _output = run(&mut program, &mut offset, vec![1]).output;
+    }
+
+    #[test]
+    fn test_read_params() {
+        let program = parse(String::from("3,0,4,0,99"));
+        let op = get_opcode(program[0]);
+        assert_eq!(op.code, 3);
+        assert_eq!(program[op.param(&program, 0, 1) as usize], 0);
+    }
+
+    #[test]
+    fn test_read_immediate_params() {
+        let program = vec![1101, 100, -1, 4, 0];
+        let op = get_opcode(program[0]);
+        assert_eq!(op.code, 1);
+        assert_eq!(op.param(&program, 0, 1), 100);
+        assert_eq!(op.param(&program, 0, 2), -1);
+    }
+
+    #[test]
+    fn test_read_position_params() {
+        let program = vec![1002, 4, 3, 4, 33];
+        let op = get_opcode(program[0]);
+        assert_eq!(op.code, 2);
+        assert_eq!(op.param(&program, 0, 1), 33);
+        assert_eq!(op.param(&program, 0, 2), 3);
+    }
+
+    #[test]
+    fn test_opcode_parser() {
+        assert_eq!(get_opcode(1002).code, 2);
+    }
+    #[test]
+    fn test_mode1() {
+        assert_eq!(get_opcode(1002).mode1, 0);
+    }
+    #[test]
+    fn test_mode2() {
+        assert_eq!(get_opcode(1002).mode2, 1);
+    }
+    #[test]
+    fn test_mode3() {
+        assert_eq!(get_opcode(1002).mode3, 0);
+    }
+
+    #[test]
+    fn test_double_digit_opcode() {
+        assert_eq!(get_opcode(99).code, 99);
+    }
+
+    #[test]
+    fn test_equal_to_eight() {
+        let program = vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8];
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![8]).output, 1);
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![7]).output, 0);
+    }
+
+    #[test]
+    fn test_less_than_eight() {
+        let program = vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8];
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![8]).output, 0);
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![9]).output, 0);
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![3]).output, 1);
+    }
+
+    #[test]
+    fn test_equal_to_eight_im() {
+        let program = vec![3, 3, 1108, -1, 8, 3, 4, 3, 99];
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![8]).output, 1);
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![5]).output, 0);
+    }
+
+    #[test]
+    fn test_less_than_eight_im() {
+        let program = vec![3, 3, 1107, -1, 8, 3, 4, 3, 99];
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![2]).output, 1);
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![9]).output, 0);
+    }
+
+    #[test]
+    fn test_jump_pos() {
+        let program = vec![3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9];
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![0]).output, 0);
+    }
+
+    #[test]
+    fn test_jump_im() {
+        let program = vec![3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1];
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![1]).output, 1);
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![2]).output, 1);
+    }
+
+    #[test]
+    fn test_lt_8_cmp() {
+        let program = vec![
+            3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0,
+            0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4,
+            20, 1105, 1, 46, 98, 99,
+        ];
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![5]).output, 999);
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![8]).output, 1000);
+        assert_eq!(run(&mut program.clone(), &mut 0, vec![15]).output, 1001);
+    }
+}
