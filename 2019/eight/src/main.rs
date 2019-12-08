ec266aa73b449ab4e07b69405b7ce9c55f6925aa
Jesse Jaggars <jjaggars@redhat.com>
2019-12-08 13:54:43 -0600

    easier to read
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- 2019/eight/src/main.rs
+++ 2019/eight/src/main.rs
@@ -1,33 +1,6 @@
 use std::fs;
 use std::iter::FromIterator;
 
-// fn part1() {
-//     let image = fs::read_to_string("input.txt").unwrap();
-//     let width = 25;
-//     let height = 6;
-//     let layer_length = width * height;
-//     let mut lvec: Vec<_> = vec![];
-//     for offset in 0..image.len() - 1 {
-//         if offset % layer_length != 0 {
-//             continue;
-//         }
-//         let mut counter: HashMap<char, usize> = HashMap::new();
-//         for ch in image.trim().chars().skip(offset).take(layer_length) {
-//             counter.entry(ch).and_modify(|v| *v += 1).or_insert(1);
-//         }
-//         let one = counter.entry('1').or_insert(0).clone();
-//         let two = counter.entry('2').or_insert(0).clone();
-//         lvec.push((*counter.entry('0').or_insert(0), one * two));
-//         println!("layer: {}, {:?}", layer, counter);
-//         layer += 1;
-//     }
-//     lvec.sort();
-
-//     for r in lvec.iter() {
-//         println!("{}\t{}", r.0, r.1);
-//     }
-// }
-
 fn main() {
     let image = fs::read_to_string("input.txt").unwrap();
     let width = 25;
@@ -65,9 +38,9 @@ fn main() {
             .skip(row * width)
             .take(width)
             .map(|ch| match ch {
-                '0' => '#',
-                '1' => 'O',
-                '2' => '_',
+                '0' => ' ',
+                '1' => '@',
+                '2' => ' ',
                 _ => unreachable!("ut oh"),
             })
             .collect();
